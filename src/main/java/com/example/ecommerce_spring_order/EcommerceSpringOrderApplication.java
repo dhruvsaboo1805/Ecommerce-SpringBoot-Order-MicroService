549dcd8958cc8d8ecd90e107e06413c4e31ddee0
DhruvSaboo <saboodhruv789@gmail.com>
2025-07-17 14:31:51 +0530

    initial commit

---
+++ src/main/java/com/example/ecommerce_spring_order/EcommerceSpringOrderApplication.java
@@ -0,0 +1,19 @@
+package com.example.ecommerce_spring_order;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class EcommerceSpringOrderApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+		SpringApplication.run(EcommerceSpringOrderApplication.class, args);
+	}
+
+}
