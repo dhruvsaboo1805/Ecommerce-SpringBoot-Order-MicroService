c1af23823452957709a282d94431fbf73c32d27b
DhruvSaboo <saboodhruv789@gmail.com>
2025-07-24 18:23:01 +0530

    eureka server integrated

--- src/main/java/com/example/ecommerce_spring_order/clients/ProductServiceClient.java
+++ src/main/java/com/example/ecommerce_spring_order/clients/ProductServiceClient.java
@@ -1,7 +1,6 @@
 package com.example.ecommerce_spring_order.clients;
 
 import com.example.ecommerce_spring_order.dto.ProductDTO;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -9,18 +8,15 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class ProductServiceClient {
 
-    private final RestTemplateBuilder restTemplateBuilder;
+    private final RestTemplate restTemplate;
 
-    public ProductServiceClient(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplateBuilder = restTemplateBuilder;
+    public ProductServiceClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
     public ProductDTO getProductById(Long productId) {
-        RestTemplate restTemplate = restTemplateBuilder.build();
-        String url = "http://localhost:8888/api/products/" + productId;
-
-        ResponseEntity<ProductDTO> response = restTemplate.getForEntity(url , ProductDTO.class);
-
+        String url = "http://ECOMMERCE-SPRING/api/products/" + productId;
+        ResponseEntity<ProductDTO> response = restTemplate.getForEntity(url, ProductDTO.class);
         return response.getBody();
     }
 }
